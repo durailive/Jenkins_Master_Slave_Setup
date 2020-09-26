20293b9ba43b88895184baa1e1da42914fba8052
durailive <70976438+durailive@users.noreply.github.com>
2020-09-26 13:38:32 -0500

    Update digitalocean_userdata.sh

--- digitalocean_userdata.sh
+++ digitalocean_userdata.sh
@@ -1,39 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@durailive 
-Learn Git and GitHub without any code!
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-
-
-wardviaene
-/
-jenkins-course
-32
-2241.2k
-Code
-Issues
-1
-Pull requests
-1
-Actions
-Projects
-Wiki
-Security
-Insights
-jenkins-course/jenkins-slave/digitalocean_userdata.sh
-@wardviaene
-wardviaene docker URL update
-Latest commit a9969c5 on Jun 2
- History
- 1 contributor
-31 lines (26 sloc)  896 Bytes
-  
 #!/bin/bash
 
 # install dependencies
@@ -65,16 +29,3 @@ chmod 700 /var/jenkins_home/.ssh
 chmod 600 /var/jenkins_home/.ssh/authorized_keys
 chown -R 1000:1000 /var/jenkins_home
 docker run -p 2222:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d wardviaene/jenkins-slave
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Found 2 out of 2 items
